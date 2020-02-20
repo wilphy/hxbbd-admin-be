f7249aa59cf941d6a31fdca77851e89213b4baea
wey <149201216@qq.com>
2020-02-20 21:36:03 +0800

    实现博客的删除功能

--- controller/blog.js
+++ controller/blog.js
@@ -1,6 +1,7 @@
 const Router = require("koa-router");
 const router = new Router();
 const callCloudDB = require("../utils/callCloudDB.js");
+const callCloudStorage = require("../utils/callCloudStorage");
 
 router.get("/list", async (ctx, next) => {
   const params = ctx.request.query;
@@ -12,6 +13,28 @@ router.get("/list", async (ctx, next) => {
     code: 20000,
     data: res.data
   };
+});
+
+router.post("/del", async (ctx, next) => {
+  const params = ctx.request.body;
+  // 删除blog
+  const queryBlog = `db.collection('blog').doc('${params._id}').remove()`;
+  const delBlogRes = await callCloudDB(ctx, "databasedelete", queryBlog);
+  // 删除blog-comment
+  const queryBlogComment = `db.collection('blog-comment').where({
+    blogId: '${params._id}'
+  }).remove()`;
+  const delBlogCommentRes = await callCloudDB(ctx, "databasedelete", queryBlogComment);
+  // 删除图片
+  const delImgStorageRes = await callCloudStorage.delete(ctx, params.img)
+  ctx.body = {
+    code: 20000,
+    data: {
+      delBlogRes,
+      delBlogCommentRes,
+      delImgStorageRes
+    }
+  }
 });
 
 module.exports = router;
